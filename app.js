93efd4c39d6d6b851c21e739333a29a8745cbd68
Felipe  Pontiggia <felipepontiggia@Felipes-Air.fibertel.com.ar>
2024-05-04 17:44:40 -0300

    add more limit size

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import mongoSanitize from "express-mongo-sanitize";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import hpp from "hpp";
 
 import AppError from "./utils/appError.js";
@@ -17,7 +18,8 @@ import productsRoute from "./routes/productsRoute.js";
 const app = express();
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-app.use(express.json());
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
